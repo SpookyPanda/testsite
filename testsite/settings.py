3742561314e6f5102df139cfbeab6dcaef1a4e72
erick <espm248@gmail.com>
2019-06-13 18:56:37 +0000

    added ugly login

--- testsite/settings.py
+++ testsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'testsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ STATIC_ROOT = "/static/"
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static/")
     ]
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
