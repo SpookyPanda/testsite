9cb5806decd6d9b357830ed317be908b5af8a62b
erick <espm248@gmail.com>
2019-06-17 20:46:52 +0000

    forgot to add view

--- namelist/views.py
+++ namelist/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("response working")
 
+def edit(request):
+	return HttpResponse("Response still working")
+
 # Create your views here.
