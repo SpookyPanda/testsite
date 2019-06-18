884a9cfdcc5eb7d8445d094c4e6bf468b1d6e5fe
erick <espm248@gmail.com>
2019-06-18 20:44:57 +0000

    error on bash

--- testsite/settings.py
+++ testsite/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = "/static/"
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static/")
-    ]
+#STATICFILES_DIRS=[
+ #   os.path.join(BASE_DIR,"static/")
+  #  ]
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
