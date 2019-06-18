db7ad8bed6bd0a57b53566d1c4169d0c7607aefc
erick <espm248@gmail.com>
2019-06-18 15:55:33 +0000

    emergency save

--- namelist/models.py
+++ namelist/models.py
@@ -4,3 +4,9 @@ class Persona(models.Model):
 	Name = models.CharField(max_length=200)
 	LastName = models.CharField(max_length=200)
 	StartDate = models.DateTimeField('Starting date')
+	def __str__(self):
+		return self.Name
+
+class Schedule(Persona):
+	CheckIn = models.DateTimeField('Check in')
+	CheckOut = models.DateTimeField('Check out')
