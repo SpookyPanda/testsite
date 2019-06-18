d8edb80c19f8b966e3b6b0ad8a50cde86a071ab0
erick <espm248@gmail.com>
2019-06-18 18:32:47 +0000

    idk why but it works on VM

---
+++ namelist/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Persona(models.Model):
+	Name = models.CharField(max_length=200)
+	LastName = models.CharField(max_length=200)
+	StartDate = models.DateTimeField('Starting date')
+	
+
+	def __str__(self):
+		return self.Name
+
+class Schedule(Persona):
+	CheckIn = models.DateTimeField('Check in')
+	CheckOut = models.DateTimeField('Check out')
---
+++ namelist/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("response working")
+
+def edit(request):
+	return HttpResponse("Response still working")
+
+def details(request, Persona_id):
+	return HttpResponse("You're looking at %s", Persona_id)
+
+def sched_details(request, Persona_id):
+	return	HttpResponse("you're lokking at %s's schedule", Persona_id)
+# Create your views here.
