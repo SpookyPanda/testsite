910c8155ce90b6ffe17430c58eae858d36b6decd
erick <espm248@gmail.com>
2019-06-20 20:55:54 +0000

    render works!

--- namelist/views.py
+++ namelist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Persona
 
 def index(request):
 	return HttpResponse("response working")
@@ -8,7 +9,8 @@ def edit(request):
 	return render(request, 'namelist/main_page.html')
 
 def details(request, Persona_id):
-	return HttpResponse("You're looking at %s", Persona_id)
+	personal = Persona.objects.all()
+	return render(request, 'namelist/someone.html', {'personal':personal})
 
 def sched_details(request, Persona_id):
 	return	HttpResponse("you're lokking at %s's schedule", Persona_id)
